557a5fbc8b405cd523898c65ca8e209996a86e46
zkunsang <zkunsang@gmail.com>
2021-03-04 16:57:44 +0900

    pushState

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  useEffect(() => {
+    window.onpopstate = function (event) {
+      console.log(`location: ${document.location}, state: ${event.state}`);
+    };
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => window.history.pushState("v1", "", "/page1")}>
+        page1
+      </button>
+      <button onClick={() => window.history.pushState("v2", "", "/page2")}>
+        page2
+      </button>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
