c31cada6c908f098bfcc6157e8b6466ebf6f0ea7
zkunsang <zkunsang@gmail.com>
2021-03-04 17:58:00 +0900

    add odd show

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   const [todoList, setTodoList] = useState([]);
   const [currentId, setCurrentId] = useState(1);
   const [desc, setDesc] = useState("");
+  const [showOdd, setShowOdd] = useState(false);
 
   function onAdd() {
     const todo = { id: currentId, desc: desc };
@@ -22,14 +23,16 @@ export default function App() {
     <div>
       <h3>할 일 목록</h3>
       <ul>
-        {todoList.map((item) => (
-          <li key={item.id}>
-            <span>{item.desc}</span>
-            <button data-id={item.id} onClick={onDelete}>
-              delete
-            </button>
-          </li>
-        ))}
+        {todoList
+          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
+          .map((item) => (
+            <li key={item.id}>
+              <span>{item.desc}</span>
+              <button data-id={item.id} onClick={onDelete}>
+                delete
+              </button>
+            </li>
+          ))}
       </ul>
       <input
         type="text"
@@ -39,6 +42,7 @@ export default function App() {
         }}
       />
       <button onClick={onAdd}>추가</button>
+      <button onClick={() => setShowOdd(!showOdd)}>showOdd</button>
       <button onClick={onSaveToServer}>서버 전송</button>
     </div>
   );
