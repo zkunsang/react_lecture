05d93f2ec40f0c616c44626bb0c82ab4fe15c410
zkunsang <zkunsang@gmail.com>
2021-03-04 22:15:13 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Counter from './Counter';
 
 export default function App() {
   const [seconds, setSeconds] = useState(0);
@@ -9,8 +10,14 @@ export default function App() {
     }, 1000);
   });
 
+  // component가 추가되는것은 mount 
+  // component가 제거되는것은 unmount
+  // component가 mount되면 useState의 초기값으로 할당이 된다.
+  // key를 넣으면 mount / unmount
+  // 동일하게 조건부 렌더링으로도 가능
   return (
-    <div key={seconds}>
+    <div>
+      {seconds % 2 === 0 && <Counter key={seconds} />}
       <h1 style={{ color: seconds % 2 ? "blue" : "red" }}>안녕하세요</h1>
       <h2>지금까지 {seconds}초가 지났습니다.</h2>
     </div>
