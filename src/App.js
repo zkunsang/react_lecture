7d4c3beba956500c8ba115b6fd375a79dc1849eb
zkunsang <zkunsang@gmail.com>
2021-03-04 17:48:52 +0900

    finish2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [currentId, setCurrentId] = useState(1);
+  const [desc, setDesc] = useState("");
+
+  function onAdd() {
+    const todo = { id: currentId, desc: desc };
+    setCurrentId(currentId + 1);
+    setTodoList([...todoList, todo]);
+  }
+
+  function onDelete(e) {
+    const id = Number(e.target.dataset.id);
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(newTodoList);
+  }
+  function onSaveToServer() {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>할 일 목록</h3>
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>
+            <span>{item.desc}</span>
+            <button data-id={item.id} onClick={onDelete}>
+              delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={desc}
+        onChange={(e) => {
+          setDesc(e.target.value);
+        }}
+      />
+      <button onClick={onAdd}>추가</button>
+      <button onClick={onSaveToServer}>서버 전송</button>
     </div>
   );
 }
-
-export default App;
