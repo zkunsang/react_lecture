d0c31a0843f4e0acb9e3dffcfb76e02a7eb1da6b
zkunsang <zkunsang@gmail.com>
2021-03-08 23:24:10 +0900

    5th

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Search from './search/container/Search';
 import 'antd/dist/antd.css';
 import { Route } from 'react-router-dom';
 import User from './user/container/User';
 
 export default function App() {
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName('body')[0];
+    const loadingEl = document.getElementById('init-loading');
+    bodyEl.removeChild(loadingEl);
+
+  })
   return (
     <>
       <Route exact path="/" component={Search} />
